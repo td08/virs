4de5cadaebd8f6c5a4b360234b2259a09d99e22f
td08 <td08.tx@gmail.com>
2015-07-14 23:51:22 -0500

    Updated EncryptionTest with two new projects

---
+++ EncryptionTest/AES_Client/Client.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace EncryptionTest
+{
+    class Client
+    {
+        static CngKey clientPvtKey;  // client private key
+
+        static byte[] clientPubKeyBlob; // client public key
+        static byte[] serverPubKeyBlob;   // server public key
+        static byte[] symmetricKey;     // the symetric key that we want to give to the server securely
+
+        static void Main(string[] args)
+        {
+            //create the client private and public keys
+            CreateKeys();
+
+            // initialice the server's public key we will need it later
+            serverPubKeyBlob = new byte[clientPubKeyBlob.Length];
+
+            // connect to the server and open a stream in order to comunicate with it
+            //TcpClient alice = new TcpClient("192.168.0.120", 54540);
+            TcpClient alice = new TcpClient("localhost", 54540);
+
+            var stream = alice.GetStream();
+
+            // send to the server the public key (client's public key)
+            stream.Write(clientPubKeyBlob, 0, clientPubKeyBlob.Length);
+
+            // now wait to receive the server's public key
+            stream.Read(serverPubKeyBlob, 0, serverPubKeyBlob.Length);
+
+            // create a random symetric key
+            symmetricKey = new byte[32];
+            Random r = new Random();
+            r.NextBytes(symmetricKey);
+            Console.WriteLine("Symmetric Key: " + System.Text.Encoding.UTF8.GetString(symmetricKey));
+                        
+            // Encrypt and send the symmetric key to the server using the server's public key
+            byte[] symmKeyBuffer = AliceSendsData(symmetricKey);
+            stream.Write(symmKeyBuffer, 0, symmKeyBuffer.Length);
+            Console.WriteLine("Length of symmKeyBuffer: " + symmKeyBuffer.Length);
+
+            // create message to encrypt
+            string test = "Howdy hello hello howdy my name is scout howdy hello! Howdy hello scout! Howdy howdy howdy hello!!!!";
+            byte[] message = new byte[System.Text.Encoding.UTF8.GetByteCount(test)];
+            message = System.Text.Encoding.UTF8.GetBytes(test);
+
+            //encrypt message using new symmetric key
+            byte[] encryptedData = EncryptSymmetric(message);
+            Console.WriteLine("Length of encryptedData: " + encryptedData.Length);
+
+            //send to server string containing length of encrypted message in number of bytes (up to 9999)
+            //byte[] messageLength = new byte[3]; //works
+            byte[] messageLength = new byte[4]; //having problems, encoding generating strange characters 
+            messageLength = System.Text.Encoding.UTF8.GetBytes(encryptedData.Length.ToString());
+            stream.Write(messageLength, 0, messageLength.Length);
+            Console.WriteLine("messageLength: " + System.Text.Encoding.UTF8.GetString(messageLength));
+
+            // once server knows message size, send the encrypted data to the server
+            stream.Write(encryptedData, 0, encryptedData.Length);
+
+            // not the server and client should have the same symetric key
+            Console.WriteLine("Finished");
+            Console.Read();
+        }
+
+
+        private static void CreateKeys()
+        {
+            clientPvtKey = CngKey.Create(CngAlgorithm.ECDiffieHellmanP256);
+            clientPubKeyBlob = clientPvtKey.Export(CngKeyBlobFormat.EccPublicBlob);
+        }
+
+        private static byte[] AliceSendsData(byte[] rawData)
+        {
+
+            byte[] encryptedData = null;
+
+            using (var aliceAlgorithm = new ECDiffieHellmanCng(clientPvtKey))
+            using (CngKey bobPubKey = CngKey.Import(serverPubKeyBlob,
+                  CngKeyBlobFormat.EccPublicBlob))
+            {
+                byte[] symmKey = aliceAlgorithm.DeriveKeyMaterial(bobPubKey);
+                //Console.WriteLine("Alice creates this symmetric key with " +
+                //      "Bobs public key information: {0}",
+                //      Convert.ToBase64String(symmKey));
+
+                using (var aes = new AesCryptoServiceProvider())
+                {
+                    aes.Key = symmKey;
+                    aes.GenerateIV();
+                    aes.Padding = PaddingMode.PKCS7;
+                    using (ICryptoTransform encryptor = aes.CreateEncryptor())
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        // create CryptoStream and encrypt data to send
+                        var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write);
+
+                        // write initialization vector not encrypted
+                        ms.Write(aes.IV, 0, aes.IV.Length);
+                        cs.Write(rawData, 0, rawData.Length);
+                        cs.Close();
+                        encryptedData = ms.ToArray();
+                    }
+                    aes.Clear();
+                }
+            }
+            //Console.WriteLine("Alice: message is encrypted: {0}",
+            //      Convert.ToBase64String(encryptedData)); ;
+            //Console.WriteLine();
+            return encryptedData;
+        }
+
+        private static byte[] EncryptSymmetric(byte[] cipherText)
+        {
+            Console.WriteLine("Encrypting using symmetric key...");
+
+            byte[] encryptedData = null;
+            var aes = new AesCryptoServiceProvider();
+
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+            aes.Key = symmetricKey;
+            aes.GenerateIV();
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(aes.Key, aes.IV), CryptoStreamMode.Write))
+                {
+                    ms.Write(aes.IV, 0, aes.IV.Length);
+                    cs.Write(cipherText, 0, cipherText.Length);
+                }
+
+                encryptedData = ms.ToArray();
+            }
+
+            aes.Clear();
+
+            Console.WriteLine("Alice: message is encrypted: {0}",
+                  Convert.ToBase64String(encryptedData)); ;
+            return encryptedData;
+        }
+    }
+}
---
+++ EncryptionTest/AES_Server/Server.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Net;
+using System.IO;
+
+namespace EncryptionTest
+{
+    class Server
+    {
+        static TcpListener server;
+
+        static CngKey bobKey;          // server private key
+        static byte[] alicePubKeyBlob; // client public key
+        static byte[] bobPubKeyBlob;   // server public key
+
+        static byte[] symmetricKey;     // the symetric key that will later be used to transfer data more efficeintly
+
+        static void Main(string[] args)
+        {
+
+            // create server private and public keys
+            CreateKeys(); 
+
+            // start listening for new connections
+            //IPAddress ipAddress = IPAddress.Parse("192.168.0.120");
+            //server = new TcpListener(ipAddress, 54540);
+
+            IPAddress ipAddress = null;
+            foreach (IPAddress ip in Dns.GetHostEntry("localhost").AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ipAddress = ip;
+                }
+            }
+            server = new TcpListener(ipAddress, 54540);
+
+
+            server.Start();
+            Console.WriteLine("Starting server...");
+            var client = server.AcceptTcpClient();
+
+            // once a connection is established open the stream
+            var stream = client.GetStream();
+
+            // we need the client public key so we need to instantiate it.
+            alicePubKeyBlob = new byte[bobPubKeyBlob.Length];
+
+            // waint until the client send us his public key
+            stream.Read(alicePubKeyBlob, 0, alicePubKeyBlob.Length);
+
+            // alicePubKeyBlob should now be the client's public key
+
+            // now let's send this servers public key to the client
+            stream.Write(bobPubKeyBlob, 0, bobPubKeyBlob.Length);
+
+            // wait for client to send encrypted symmetric key
+            byte[] symmKeyBuffer = new byte[64];
+            stream.Read(symmKeyBuffer, 0, symmKeyBuffer.Length);
+
+            //decrypt the symmetric key with the private key of the server
+            symmetricKey = BobReceivesData(symmKeyBuffer);
+            Console.WriteLine("Symmetric Key: " + System.Text.Encoding.UTF8.GetString(symmetricKey));
+
+            // wait for client to send encrypted message length for buffer size
+            //byte[] dataLength = new byte[3]; //works
+            byte[] dataLength = new byte[4]; //having problems, encoding generating strange characters 
+            Console.WriteLine("dataLength: " + System.Text.Encoding.UTF8.GetString(dataLength));
+
+            stream.Read(dataLength, 0, dataLength.Length);
+            Console.WriteLine("dataLength: " + System.Text.Encoding.UTF8.GetString(dataLength));
+            int messageLength = Int32.Parse(System.Text.Encoding.UTF8.GetString(dataLength));
+            //int messageLength = Convert.ToInt32(System.Text.Encoding.UTF8.GetString(dataLength));
+            Console.WriteLine("Client sends message length: " + messageLength);
+
+            // encryptedData will be the data that server will recive encrypted from the client with the server's public key
+            byte[] encryptedData = new byte[messageLength];
+
+            // wait until client sends that data
+            stream.Read(encryptedData, 0, encryptedData.Length);
+
+            // decrypt message from client using symmetric key
+            string message = DecryptSymmetric(encryptedData);
+            Console.WriteLine("Decrypted message: " + message);
+
+            // server and client should know have the same symetric key in order to send data more efficently and securely
+            Console.WriteLine("Finished");
+            Console.Read();
+
+        }
+
+        private static void CreateKeys()
+        {
+            //aliceKey = CngKey.Create(CngAlgorithm.ECDiffieHellmanP256);
+            bobKey = CngKey.Create(CngAlgorithm.ECDiffieHellmanP256);
+            //alicePubKeyBlob = aliceKey.Export(CngKeyBlobFormat.EccPublicBlob);
+            bobPubKeyBlob = bobKey.Export(CngKeyBlobFormat.EccPublicBlob);
+        }
+
+        private static byte[] BobReceivesData(byte[] encryptedData)
+        {
+            Console.WriteLine("Bob receives encrypted data");
+            byte[] rawData = null;
+
+            var aes = new AesCryptoServiceProvider();
+
+            int nBytes = aes.BlockSize >> 3;
+            Console.WriteLine("nBytes: " + nBytes + "\nBlock size: " + aes.BlockSize);
+            byte[] iv = new byte[nBytes];
+            for (int i = 0; i < iv.Length; i++)
+                iv[i] = encryptedData[i];
+
+            using (var bobAlgorithm = new ECDiffieHellmanCng(bobKey))
+            using (CngKey alicePubKey = CngKey.Import(alicePubKeyBlob,
+                  CngKeyBlobFormat.EccPublicBlob))
+            {
+                byte[] symmKey = bobAlgorithm.DeriveKeyMaterial(alicePubKey);
+                //Console.WriteLine("Bob creates this symmetric key with " +
+                //      "Alices public key information: {0}",
+                //      Convert.ToBase64String(symmKey));
+
+                aes.Key = symmKey;
+                aes.IV = iv;
+                aes.Padding = PaddingMode.PKCS7;
+
+                using (ICryptoTransform decryptor = aes.CreateDecryptor())
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Write);
+                    cs.Write(encryptedData, nBytes, encryptedData.Length - nBytes);
+                    cs.FlushFinalBlock();
+                    cs.Close();
+
+                    rawData = ms.ToArray();
+
+                    //Console.WriteLine("Bob decrypts message to: {0}",
+                    //      Encoding.UTF8.GetString(rawData));
+                }
+                aes.Clear();
+
+                return rawData;
+            }
+        }
+
+        private static string DecryptSymmetric(byte[] cipherText)
+        {
+            Console.WriteLine("Decrypting using symmetric key...");
+
+            byte[] rawData = null;
+            var aes = new AesCryptoServiceProvider();
+            byte[] iv = new byte[16];
+            for (int i = 0; i < iv.Length; i++)
+                iv[i] = cipherText[i];
+
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(symmetricKey, iv), CryptoStreamMode.Write))
+                {
+                    cs.Write(cipherText, 16, cipherText.Length - 16);
+                    cs.FlushFinalBlock();
+                }
+
+                rawData = ms.ToArray();
+            }
+
+            aes.Clear();
+
+            return System.Text.Encoding.UTF8.GetString(rawData);
+        }
+    }
+}
